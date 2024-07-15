f9b859df5ede351d74d114cef6a55cbf3b03e883
Akshay Suresh <akshay721@gmail.com>
2024-07-15 22:52:20 +0530

    Added test for checking response to valid AWS region name

--- amazon_seg_project_tests/test_aws_credentials.py
+++ amazon_seg_project_tests/test_aws_credentials.py
@@ -2,7 +2,9 @@
 Unit-test modules defined in scripts/aws_credentials.py
 """
 
+import random
 import unittest.mock as mock
+from amazon_seg_project.scripts.constants import AWS_REGIONS_LIST
 from amazon_seg_project.scripts.aws_credentials import get_aws_region_name
 
 
@@ -19,3 +21,12 @@ def test_get_aws_region_name_invalidname() -> None:
                 str(e)
                 == f"AWS region name '{mock_region}' is not valid or not supported."
             )
+
+
+def test_get_aws_region_name_valid() -> None:
+    """
+    Tests that get_aws_region_name() responds correctly to a region specified in AWS_REGIONS_LIST
+    """
+    mock_region = random.choice(AWS_REGIONS_LIST)
+    with mock.patch("os.getenv", return_value=mock_region):
+        assert mock_region == get_aws_region_name()
