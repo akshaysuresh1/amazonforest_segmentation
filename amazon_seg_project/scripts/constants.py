67e424643691cee63231634cd9eb95b4fc776f0b
Akshay Suresh <akshay721@gmail.com>
2024-07-16 16:30:59 +0530

    Incorporating reviewer suggestions

--- amazon_seg_project/scripts/constants.py
+++ amazon_seg_project/scripts/constants.py
@@ -2,43 +2,20 @@
 Values of global variables
 """
 
-import os
+import boto3
+from pathlib import Path
 
 # Path to training data and labels
-TRAINING_IMAGES_PREFIX = os.path.join("train", "images")
-TRAINING_MASKS_PREFIX = os.path.join("train", "masks")
+TRAINING_IMAGES_PREFIX = Path("train") / "images"
+TRAINING_MASKS_PREFIX = Path("train") / "masks"
 
 # Paths to validation data and labels
-VALIDATION_IMAGES_PREFIX = os.path.join("validation", "images")
-VALIDATION_MASKS_PREFIX = os.path.join("validation", "masks")
+VALIDATION_IMAGES_PREFIX = Path("validation") / "images"
+VALIDATION_MASKS_PREFIX = Path("validation") / "masks"
 
 # Paths to test data
-TEST_IMAGES_PREFIX = os.path.join("test", "images")
-TEST_MASKS_PREFIX = os.path.join("test", "masks")
+TEST_IMAGES_PREFIX = Path("test") / "images"
+TEST_MASKS_PREFIX = Path("test") / "masks"
 
 # Allowed AWS region names
-AWS_REGIONS_LIST = [
-    "us-east-1",  # US East (N. Virginia)
-    "us-east-2",  # US East (Ohio)
-    "us-west-1",  # US West (N. California)
-    "us-west-2",  # US West (Oregon)
-    "af-south-1",  # Africa (Cape Town)
-    "ap-east-1",  # Asia Pacific (Hong Kong)
-    "ap-south-1",  # Asia Pacific (Mumbai)
-    "ap-northeast-3",  # Asia Pacific (Osaka-Local)
-    "ap-northeast-2",  # Asia Pacific (Seoul)
-    "ap-southeast-1",  # Asia Pacific (Singapore)
-    "ap-southeast-2",  # Asia Pacific (Sydney)
-    "ap-northeast-1",  # Asia Pacific (Tokyo)
-    "ca-central-1",  # Canada (Central)
-    "cn-north-1",  # China (Beijing) - Operated by Sinnet
-    "cn-northwest-1",  # China (Ningxia) - Operated by NWCD
-    "eu-central-1",  # Europe (Frankfurt)
-    "eu-west-1",  # Europe (Ireland)
-    "eu-west-2",  # Europe (London)
-    "eu-south-1",  # Europe (Milan)
-    "eu-west-3",  # Europe (Paris)
-    "eu-north-1",  # Europe (Stockholm)
-    "me-south-1",  # Middle East (Bahrain)
-    "sa-east-1",  # South America (São Paulo)
-]
+AWS_REGIONS_LIST = boto3.session.Session().get_available_regions('s3')
--- amazon_seg_project_tests/test_aws_credentials.py
+++ amazon_seg_project_tests/test_aws_credentials.py
@@ -4,6 +4,7 @@ Unit-test modules defined in scripts/aws_credentials.py
 
 import random
 import unittest.mock as mock
+import pytest
 from amazon_seg_project.scripts.constants import AWS_REGIONS_LIST
 from amazon_seg_project.scripts.aws_credentials import get_aws_region_name
 
@@ -13,14 +14,13 @@ def test_get_aws_region_name_invalidname() -> None:
     Tests the response of get_aws_region_name() to an invalid AWS region name.
     """
     mock_region = "asia"
-    with mock.patch("os.getenv", return_value=mock_region):
-        try:
+    with pytest.raises(ValueError) as e:
+        with mock.patch("os.getenv", return_value=mock_region):
             get_aws_region_name()
-        except ValueError as e:
-            assert (
-                str(e)
-                == f"AWS region name '{mock_region}' is not valid or not supported."
-            )
+    assert (
+        str(e.value)
+        == f"AWS region name '{mock_region}' is not valid or not supported."
+    )
 
 
 def test_get_aws_region_name_valid() -> None:
