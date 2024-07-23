9c5bc8f34743cb6001197b3ab24b363e106ce0a6
Akshay Suresh <akshay721@gmail.com>
2024-07-24 02:26:13 +0530

    Updated version requirements

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="amazon_seg_project",
     packages=find_packages(exclude=["amazon_seg_project_tests"]),
     install_requires=[
-        "boto3==1.34.145",
-        "rioxarray==0.17.0",
-        "xarray==2024.6.0",
+        "boto3>=1.34,<1.35",
+        "rioxarray>=0.17,<0.18",
+        "xarray>=2024.6,<2024.7",
     ],
-    extras_require={"dev": ["pytest==8.2.2"]},
+    extras_require={"dev": ["pytest>=8.2,<8.3"]},
 )
