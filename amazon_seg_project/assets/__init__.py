153c20e5bd81f22753248cfe8b3cc935506e2c71
Akshay Suresh <akshay721@gmail.com>
2025-05-24 23:09:36 +0100

    Intoduced asset and unit test for test dataset

--- amazon_seg_project/assets/__init__.py
+++ amazon_seg_project/assets/__init__.py
@@ -8,7 +8,7 @@ from .data_products import (
     data_test,
 )
 from .dataset_definition import SegmentationDataset
-from .datasets import afs_training_dataset, afs_validation_dataset
+from .datasets import afs_training_dataset, afs_validation_dataset, afs_test_dataset
 from .model import unet_model, trained_unet_model
 from .precision_recall_metrics import precision_recall_curve
 from .statistics import channel_stats_training_data
@@ -20,6 +20,7 @@ __all__ = [
     "SegmentationDataset",
     "afs_training_dataset",
     "afs_validation_dataset",
+    "afs_test_dataset",
     "unet_model",
     "trained_unet_model",
     "precision_recall_curve",
--- amazon_seg_project/assets/datasets.py
+++ amazon_seg_project/assets/datasets.py
@@ -36,7 +36,7 @@ def afs_training_dataset(
         ),
     )
     yield Output(
-        training_dataset, metadata={"Training dataset length": len(training_dataset)}
+        training_dataset, metadata={"Training dataset size": len(training_dataset)}
     )
 
 
@@ -50,6 +50,10 @@ def afs_validation_dataset(
 ) -> Generator[Output, None, None]:
     """
     Validation dataset for Amazon forest segmentation
+
+    Args:
+        val_images_files: List of validation image filenames (including paths)
+        val_mask_files: List of validation mask filenames (including paths)
     """
     validation_dataset = SegmentationDataset(
         images_list=val_image_files,
@@ -60,5 +64,32 @@ def afs_validation_dataset(
     )
     yield Output(
         validation_dataset,
-        metadata={"Validation dataset length": len(validation_dataset)},
+        metadata={"Validation dataset size": len(validation_dataset)},
+    )
+
+
+@asset(
+    name="test_dataset",
+    ins={"test_image_files": AssetIn(), "test_mask_files": AssetIn()},
+)
+def afs_test_dataset(
+    test_image_files: List[str], test_mask_files: List[str]
+) -> Generator[Output, None, None]:
+    """
+    Test dataset for Amazon forest segmentation
+
+    Args:
+        test_images_files: List of image filenames (including paths) in test dataset
+        test_mask_files: List of mask filenames (including paths) in test dataset
+    """
+    test_dataset = SegmentationDataset(
+        images_list=test_image_files,
+        masks_list=test_mask_files,
+        s3_bucket=AMAZON_TIF_BUCKET.get_value() or "",
+        scaling_func=robust_scaling,
+        transform=None,
+    )
+    yield Output(
+        test_dataset,
+        metadata={"Test dataset size": len(test_dataset)},
     )
